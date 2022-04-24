a8172c4207303df0eb555496ebe4231e221c8f41
wzhixin <wzhixin@outlook.com>
2022-04-24 10:08:33 +0800

    add

---
+++ py100days_local_lab.py
@@ -0,0 +1,28 @@
+print ("D1 input")
+
+import turtle
+    
+turtle.pensize(4)
+turtle.pencolor('red')
+    
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
+
+
+
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a))    # <class 'int'>
+print(type(b))    # <class 'float'>
+print(type(c))    # <class 'complex'>
+print(type(d))    # <class 'str'>
+print(type(e))    # <class 'bool'>
